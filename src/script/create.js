297b63abbd75db456c46b6b4eb7c1eba543225e2
Даниил <fuzeev.daniil@yandex.ru>
2022-05-09 21:11:01 +0300

    deploy task

--- src/script/create.js
+++
@@ -1,7 +0,0 @@
-export default function create(tag, className, parent, textContent) {
-  const elem = document.createElement(tag);
-  if (className) elem.classList.add(className);
-  if (parent) parent.append(elem);
-  if (textContent) elem.textContent = textContent;
-  return elem;
-}
